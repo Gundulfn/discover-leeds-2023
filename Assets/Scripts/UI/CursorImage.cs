1d7841e88416d8b4ca77032c6de30607151cc6b1
Gundulfn <burakcanciftci1@gmail.com>
2021-09-29 16:12:41 +0300

    Cursor image added

---
+++ Assets/Scripts/UI/CursorImage.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class CursorImage: MonoBehaviour
+{
+    public Texture2D crossHairTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+ 
+    void Update()
+    {
+        if(!UIHandler.isUIActive)
+        {
+            Cursor.SetCursor(crossHairTexture, hotSpot, cursorMode);
+        }
+        else
+        {
+            Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        }
+    }
+}
